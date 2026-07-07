ad17274ba3c077fe2a7fd794239ed82677c8d672
OPAL ERP <abbas.banisalameh@gmail.com>
2026-07-07 15:31:09 +0000

    OPAL ERP: Add dynamic school and user identity

--- config/settings.py
+++ config/settings.py
@@ -88,6 +88,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'announcements.context_processors.active_announcement',
+                'core.context_processors.opal_identity',
             ],
         },
     },
